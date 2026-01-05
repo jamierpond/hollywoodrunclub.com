1155ce4d50b582d114990dbb5605c11d612e0d41
Jamie Pond <jamiepond259@gmail.com>
2026-01-05 00:12:45 -0800

    favicon

---
+++ app/favicon.ico/route.ts
@@ -0,0 +1,7 @@
+import { generateIcon } from "@/lib/icon";
+
+export const runtime = "nodejs";
+
+export async function GET() {
+  return generateIcon(256);
+}
---
+++ lib/icon.tsx
@@ -0,0 +1,49 @@
+import { ImageResponse } from "next/og";
+
+const FONT_URL =
+  "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuFuYMZg.ttf";
+
+export async function generateIcon(size = 256) {
+  const interBlack = await fetch(FONT_URL).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#000",
+          borderRadius: `${Math.round(size * 0.2)}px`,
+        }}
+      >
+        <span
+          style={{
+            fontSize: `${Math.round(size * 0.59)}px`,
+            fontFamily: "Inter",
+            fontWeight: 900,
+            color: "#fff",
+            letterSpacing: "-0.06em",
+            marginTop: `${Math.round(size * -0.03)}px`,
+          }}
+        >
+          H
+        </span>
+      </div>
+    ),
+    {
+      width: size,
+      height: size,
+      fonts: [
+        {
+          name: "Inter",
+          data: interBlack,
+          style: "normal",
+          weight: 900,
+        },
+      ],
+    }
+  );
+}
